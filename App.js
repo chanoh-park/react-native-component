34a052e746f387ba69a52c792f0781d5a789fddd
pco <pco@paywith.co.kr>
2021-09-05 16:46:22 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const name = "Chano";
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        {(() => {
+          if (name === "Hanbit") return "My name is Hanbit";
+          else if (name === "Chano") return "My name is Chano";
+          else return "My name is React Native";
+        })()}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
   },
 });
