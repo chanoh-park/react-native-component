dbd2968cb4f684b4292d5c2d57e5ac1f69cb05e0
pco <pco@paywith.co.kr>
2021-09-09 15:19:35 +0900

    버튼 만들기

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import MyButton from "./components/MyButton";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My Button Component</Text>
-      <MyButton title="Button" />
-      <MyButton title="Button">Children Props</MyButton>
-      <MyButton />
+      <Text style={styles.text}>Props</Text>
+      <MyButton title="Button" onPress={() => alert("props")} />
+      <MyButton title="Button" onPress={() => alert("children")}>
+        Children Props
+      </MyButton>
+      <MyButton title="Button" onPress={() => alert("default")} />
     </View>
   );
 };
--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import PropTypes from "prop-types";
 
 const MyButton = (props) => {
   return (
     <TouchableOpacity
       style={styles.touchableOpacity}
-      onPress={() => alert("Click !!!")}
+      onPress={() => props.onPress()}
     >
       <Text style={styles.text}>{props.children || props.title}</Text>
     </TouchableOpacity>
   );
 };
 
-MyButton.defaultProps = {
-  title: "Button",
+MyButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
